833fb9acdecf77e00a0ee02b994bcd30b7eeeee4
Mahmoud <cumody82@gmail.com>
2021-10-01 01:38:43 +0200

    adding ktlint

--- app/src/test/java/com/mahmoudshaaban/prox/ExampleUnitTest.kt
+++ app/src/test/java/com/mahmoudshaaban/prox/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
 package com.mahmoudshaaban.prox
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
